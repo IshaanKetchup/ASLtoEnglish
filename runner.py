a3b8adc9d939532d622624c6f620e26ed41beed9
Ishaan Kashyap <88713875+IshaanKetchup@users.noreply.github.com>
2023-09-20 04:31:49 +0530

    Update runner.py
    Minor changes

--- runner.py
+++ runner.py
@@ -21,7 +21,6 @@ def fob(handpoints, image):
             cv2.putText(image,"FRONT",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
 
 def letter(handpoints, image):
-
     if(handpoints[2].y > handpoints[3].y and
        handpoints[3].y > handpoints[4].y and
        handpoints[3].x > handpoints[7].x and
@@ -50,39 +49,6 @@ def letter(handpoints, image):
        handpoints[16].y < handpoints[14].y and
        handpoints[20].y < handpoints[18].y):
         cv2.putText(image,"B",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
-
-
-    if (
-        handpoints[4].x < handpoints[3].x and
-        handpoints[3].x < handpoints[2].x and
-        handpoints[6].y < handpoints[5].y and
-        handpoints[7].y < handpoints[6].y and
-        handpoints[8].y < handpoints[7].y and
-        handpoints[15].y < handpoints[16].y and
-        handpoints[13].y < handpoints[16].y and
-        handpoints[10].y < handpoints[9].y and
-        handpoints[12].y < handpoints[10].y
-        ):
-        if((handpoints[10].x < handpoints[9].x and handpoints[11].x < handpoints[9].x and handpoints[12].x < handpoints[9].x) and
-        (handpoints[5].x < handpoints[6].x and handpoints[5].x < handpoints[7].x and handpoints[5].x < handpoints[8].x)):
-            cv2.putText(image,"V",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
-        else:
-            cv2.putText(image, "U", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
-
-        
-    if(handpoints[2].y > handpoints[3].y and
-        handpoints[3].y > handpoints[4].y and
-        handpoints[8].x > handpoints[4].x and
-        handpoints[8].y > handpoints[7].y and
-        handpoints[15].y > handpoints[13].y and
-        handpoints[13].y > handpoints[14].y and
-        handpoints[11].y > handpoints[9].y and
-        handpoints[9].y > handpoints[10].y and
-        handpoints[19].y > handpoints[17].y and
-        handpoints[17].y > handpoints[18].y and
-        handpoints[12].y > handpoints[2].y and
-        handpoints[16].y > handpoints[2].y):
-            cv2.putText(image,"T",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
 
     if(
     #thumb.x
@@ -122,95 +88,6 @@ def letter(handpoints, image):
     handpoints[19].y > handpoints[18].y and
     handpoints[17].y > handpoints[18].y):
         cv2.putText(image,"C",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
-    
-    if(
-        handpoints[8].y>handpoints[5].y and
-        handpoints[8].y>handpoints[7].y and
-        handpoints[8].y>handpoints[6].y and
-        handpoints[7].y>handpoints[6].y and
-
-        handpoints[12].y<handpoints[11].y and
-        handpoints[11].y<handpoints[10].y and
-        handpoints[10].y<handpoints[9].y and
-        
-        handpoints[16].y<handpoints[15].y and
-        handpoints[15].y<handpoints[14].y and
-        handpoints[14].y<handpoints[13].y and
-        
-        handpoints[20].y<handpoints[19].y and
-        handpoints[19].y<handpoints[18].y and
-        handpoints[18].y<handpoints[17].y and
-
-        handpoints[4].x<handpoints[3].x and
-        handpoints[3].x<handpoints[2].x and
-        handpoints[2].x<handpoints[1].x 
-
-        #do y
-        
-    ):
-        cv2.putText(image,"F",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
-    
-    if(handpoints[5].x<handpoints[6].x and 
-       handpoints[6].x<handpoints[7].x and
-
-       handpoints[5].y<handpoints[9].y and
-
-       handpoints[2].y<handpoints[4 ].y and
-
-       handpoints[9].x<handpoints[10].x and
-       handpoints[11].x<handpoints[12].x and
-
-       handpoints[0].y<handpoints[17].y):
-        cv2.putText(image,"H",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
-
-    if(handpoints[8].y<handpoints[6].y and
-       handpoints[6].y<handpoints[5].y and
-       
-       handpoints[12].y<handpoints[11].y and
-       handpoints[11].y<handpoints[9].y and
-       
-       handpoints[4].x>handpoints[9].x and
-       handpoints[4].x<handpoints[5].x and
-       
-       handpoints[15].y>handpoints[14].y and
-       handpoints[13].y>handpoints[14].y and
-       
-       handpoints[4].y<handpoints[9].y and
-       
-       handpoints[20].y>handpoints[18].y and
-       handpoints[16].y>handpoints[14].y and
-       handpoints[19].y>handpoints[17].y and
-       handpoints[15].y>handpoints[13].y):
-        cv2.putText(image,"K",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
-    
-    if(handpoints[4].x > handpoints[3].x and
-       handpoints[3].x > handpoints[2].x and
-       handpoints[17].y > handpoints[18].y and
-       handpoints[18].y > handpoints[19].y and
-       handpoints[19].y > handpoints[20].y and
-       handpoints[13].y > handpoints[14].y and
-       handpoints[15].y > handpoints[13].y and
-       handpoints[9].y > handpoints[10].y and
-       handpoints[11].y > handpoints[9].y and
-       handpoints[5].y > handpoints[6].y and
-       handpoints[7].y > handpoints[5].y):
-       cv2.putText(image,"Y",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
-    
-    if(handpoints[3].x > handpoints[4].x and
-       handpoints[2].y > handpoints[3].y and
-       handpoints[8].x > handpoints[7].x and
-       handpoints[7].x > handpoints[6].x and
-       handpoints[7].y > handpoints[8].y and
-       handpoints[10].x > handpoints[9].x and
-       handpoints[11].x > handpoints[10].x and
-       handpoints[10].x > handpoints[12].x and
-       handpoints[14].x > handpoints[13].x and
-       handpoints[15].x > handpoints[14].x and
-       handpoints[14].x > handpoints[16].x and
-       handpoints[18].x > handpoints[17].x and
-       handpoints[19].x > handpoints[18].x and
-       handpoints[18].x > handpoints[20].x):
-        cv2.putText(image,"X",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
 
     if(handpoints[3].x > handpoints[4].x and
        handpoints[2].x > handpoints[3].x and
@@ -224,40 +101,8 @@ def letter(handpoints, image):
        handpoints[17].y > handpoints[19].y and
        handpoints[19].y > handpoints[18].y):
         cv2.putText(image,"E",(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
+
     
-    if (handpoints[4].x > handpoints[3].x and
-            handpoints[3].x > handpoints[2].x and
-            handpoints[0].x > handpoints[17].x and
-
-            handpoints[6].y < handpoints[5].y and
-            handpoints[7].y < handpoints[6].y and
-            handpoints[8].y < handpoints[7].y and
-
-            handpoints[10].y < handpoints[9].y and
-            handpoints[9].y < handpoints[12].y and
-            handpoints[14].y < handpoints[13].y and
-            handpoints[13].y < handpoints[16].y and
-
-            handpoints[19].y < handpoints[20].y and
-            handpoints[17].y < handpoints[20].y):
-        cv2.putText(image, "L", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,255),1)
-
-    if (handpoints[3].x > handpoints[4].x and
-            handpoints[2].x > handpoints[3].x and
-            handpoints[0].x > handpoints[17].x and
-
-            handpoints[6].y < handpoints[5].y and
-            handpoints[7].y < handpoints[6].y and
-            handpoints[8].y < handpoints[7].y and
-
-            handpoints[10].y < handpoints[9].y and
-            handpoints[12].y < handpoints[10].y and
-            handpoints[14].y < handpoints[13].y and
-            handpoints[16].y < handpoints[14].y and
-
-            handpoints[19].y < handpoints[20].y and
-            handpoints[17].y < handpoints[20].y):
-        cv2.putText(image, "W", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,255),1)
     
 def which_hand():
     for i in results.multi_handedness:
